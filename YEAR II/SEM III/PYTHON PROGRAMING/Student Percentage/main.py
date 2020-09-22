da6029e8296ff9742f33190170cdb9f543162278
Shubhang Gupta <shubhsahu1805@gmail.com>
2020-09-22 19:26:36 +0000

    Updated the code

--- YEAR II/SEM III/PYTHON PROGRAMING/Student Percentage/main.py
+++ YEAR II/SEM III/PYTHON PROGRAMING/Student Percentage/main.py
@@ -1,11 +1,34 @@
-def main():
-    dict = {}
-    for i in range(1, 5, 1):
-        name = input("Enter the name: ")
+def maximum(dic):
+    maxim = 0.0
+    for i in dic:
+        if dic[i]['Percentage'] > maxim:
+            maxim = dic[i]['Percentage']
+
+    return i, maxim
+
+def percentage(dic):
+    for i in dic:
+        total = 0
         for j in range(1, 5, 1):
-            marks = int(input(f"Marks in subject {j}: "))
-            dict[name][f"Subject {j}"] = marks
-    print(dict)
+            total += dic[i][f'Subject {j}']
+        dic[i]['Percentage'] = total/4
+    
+    print(dic)
+    name, maxim = maximum(dic)
+    print(f"{name} has got the highest percentage of {maxim}")
+
+def main():
+    dic = {}
+    marks = {}
+    n = int(input("Enter the no. of students: "))
+    for i in range(1, n+1, 1):
+        name = input("\nEnter the name: ")
+        name = name.title()
+        dic[name] = marks
+        for j in range(1, 5, 1):
+            mark = int(input(f"Marks in subject {j}: "))
+            dic[name][f"Subject {j}"] = mark
+    percentage(dic)
 
 if __name__ == "__main__":
     main()
