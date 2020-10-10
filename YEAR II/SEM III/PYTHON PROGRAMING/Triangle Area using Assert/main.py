b3fb7dfbfc3ed606be504e38dbb21c9f45ceb55b
Shubhang Gupta <shubhsahu1805@gmail.com>
2020-10-10 03:28:54 +0000

    Added perimeter functionality

--- YEAR II/SEM III/PYTHON PROGRAMING/Triangle Area using Assert/main.py
+++ YEAR II/SEM III/PYTHON PROGRAMING/Triangle Area using Assert/main.py
@@ -18,14 +18,16 @@ def calc_area(a, b, c):
     assert (b+c)>a, "You entered wrong sides!"
     s = (a+b+c)/2
     area = (s*(s-a)*(s-b)*(s-c))**0.5
-    return area
+    perimeter = a+b+c
+    return area, perimeter
 
 def main():
     a = int(input("Enter the side1 : "))
     b = int(input("Enter the side2 : "))
     c = int(input("Enter the side3 : "))
-    area = calc_area(a, b, c)
+    area, perimeter = calc_area(a, b, c)
     print("\nThe area of triangle is : %0.2f" %area)
+    print("\nThe perimter of triangle is : %0.2f" %perimeter)
 
 if __name__ == "__main__":
     main()
