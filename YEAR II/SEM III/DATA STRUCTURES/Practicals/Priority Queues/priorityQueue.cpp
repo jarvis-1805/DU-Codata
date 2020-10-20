630b8bda51daf8e87977c6099b18573759d69453
Shubhang Gupta <shubhsahu1805@gmail.com>
2020-10-20 12:08:03 +0000

    Added heap sort funtionality;

--- YEAR II/SEM III/DATA STRUCTURES/Practicals/Priority Queues/priorityQueue.cpp
+++ YEAR II/SEM III/DATA STRUCTURES/Practicals/Priority Queues/priorityQueue.cpp
@@ -36,6 +36,8 @@ class priorityQueue
         void min_heap_insert(int *, int);
         int heap_minimum(int *);
         int heap_extract_min(int *);
+        void heap_sort(int *);
+        void display();
 };
 
 priorityQueue::priorityQueue()
@@ -43,21 +45,21 @@ priorityQueue::priorityQueue()
     cout << "\n=========== PRIORITY QUEUE ===========\n";
     cout << "\nEnter the size: ";
     cin >> size;
-    cout << "\nEnter the elements: ";
-    for(int i=1; i<=size; i++)
-        cin >> array[i];
+    if(size != 0)
+    {
+        cout << "\nEnter the elements: ";
+        for(int i=1; i<=size; i++)
+            cin >> array[i];
+    }
+    system("clear");
 }
 
 void priorityQueue::options()
 {
 	cout << "\n------- MENU -------";
-    cout << "\n1. MAX HEAPIFY"
-		<<	"\n2. MIN HEAPIFY"
-		<<	"\n3. BUILD MAX HEAP"
-		<<	"\n4. BUILD MIN HEAP"
-		<<	"\n5. MAX PRIORITY QUEUE"
-		<<	"\n6. MIN PRIORITY QUEUE"
-		<<	"\n7. DISPLAY"
+    cout <<	"\n1. MAX PRIORITY QUEUE"
+		<<	"\n2. MIN PRIORITY QUEUE"
+		<<	"\n3. HEAP SORT"
 		<<	"\n0. EXIT";
 }
 
@@ -74,50 +76,45 @@ void priorityQueue::choiceCalling(int ch)
     switch(ch)
 	{
 		case 1:
-			cout << "\nEnter index from " << floor(size/2) << " upto 1: ";
-		    cin >> i;
-            if(i >= floor(size/2) && i <=1)
+            system("clear");
+            do
             {
-                max_heapify(array, i);
-                cout << "\nSuccessfully Max Heapified at " << i << ".\n";
-            }
-            else
-                cout << "\n########### WRONG CHOICE... ###########\n";
+                cout << "\n=========== MAX PRIORITY QUEUE ===========\n";
+                sub_options_1();
+                ch1 = choice();
+                if(ch1 == 0)
+                    break;
+                system("clear");
+                cout << "\n=========== MAX PRIORITY QUEUE ===========\n";
+                sub_choiceCalling_1(ch1);
+                cout << "\nPress Enter to continue...";
+                cin.ignore();
+                getchar();
+		        system("clear");
+            }while(ch1 != 0);
 			break;
 		case 2:
-			cout << "\nEnter index from " << floor(size/2) << " upto 1: ";
-		    cin >> i;
-            if(i >= floor(size/2) && i <=1)
+            system("clear");
+            do
             {
-                min_heapify(array, i);
-                cout << "\nSuccessfully Min Heapified at " << i << ".\n";
-            }
-            else
-                cout << "\n########### WRONG CHOICE... ###########\n";
+                cout << "\n=========== MIN PRIORITY QUEUE ===========\n";
+                sub_options_2();
+                ch1 = choice();
+                if(ch1 == 0)
+                    break;
+                system("clear");
+                cout << "\n=========== MIN PRIORITY QUEUE ===========\n";
+                sub_choiceCalling_2(ch1);
+                cout << "\nPress Enter to continue...";
+                cin.ignore();
+                getchar();
+		        system("clear");
+            }while(1);
 			break;
 		case 3:
-			build_max_heap();
-            cout << "\nSuccessfully built Max Heap.\n";
-			break;
-		case 4:
-			build_min_heap();
-            cout << "\nSuccessfully built Min Heap.\n";
-			break;
-		case 5:
-            sub_options_1();
-            ch1 = choice();
-			sub_choiceCalling_1(ch1);
-			break;
-		case 6:
-            sub_options_2();
-            ch1 = choice();
-			sub_choiceCalling_2(ch1);
-			break;
-		case 7:
-            cout << "\nArray: ";
-            for(int x=1; x<=size; x++)
-                cout << array[x] << " ";
-            cout << endl;
+            heap_sort(array);
+            cout << "\nSuccessfully Sorted Heap!\n";
+            display();
 			break;
 		case 0:
 			break;
@@ -128,11 +125,15 @@ void priorityQueue::choiceCalling(int ch)
 
 void priorityQueue::sub_options_1()
 {
-	cout << "\n------- SUB MENU -------";
-    cout << "\n1. HEAP INCREASE KEY"
-		<< "\n2. MAX HEAP INSERT"
-		<< "\n3. HEAP MAXIMUM"
-		<< "\n4. HEAP EXTRACT MAX";
+	cout << "\n------- MENU -------";
+    cout << "\n1. MAX HEAPIFY"
+		<< "\n2. BUILD MAX HEAP"
+        << "\n3. HEAP INCREASE KEY"
+		<< "\n4. MAX HEAP INSERT"
+		<< "\n5. HEAP MAXIMUM"
+		<< "\n6. HEAP EXTRACT MAX"
+		<< "\n7. DISPLAY"
+		<< "\n0. EXIT";
 }
 
 void priorityQueue::sub_choiceCalling_1(int ch1)
@@ -141,28 +142,57 @@ void priorityQueue::sub_choiceCalling_1(int ch1)
     int key;
     switch(ch1)
 	{
-		case 1:
-			cout << "\nEnter index to be increased: ";
+        case 1:
+			cout << "\nEnter index from " << floor(size/2) << " upto 1: ";
 		    cin >> i;
-			cout << "\nEnter the data: ";
-		    cin >> key;
-            heap_increase_key(array, i, key);
+            if(i <= floor(size/2) && i >= 1)
+            {
+                max_heapify(array, i);
+                cout << "\nSuccessfully Max Heapified at " << i << "!\n";
+                display();
+            }
+            else
+                cout << "\n########### WRONG CHOICE... ###########\n";
 			break;
 		case 2:
+			build_max_heap();
+            cout << "\nSuccessfully built Max Heap!\n";
+            display();
+			break;
+		case 3:
+			cout << "\nEnter index to be increased: ";
+		    cin >> i;
+            if(i <= size && i >= 1)
+            {
+                cout << "\nEnter the data: ";
+                cin >> key;
+                heap_increase_key(array, i, key);
+            }
+            else
+                cout << "\n########### WRONG CHOICE... ###########\n";
+			break;
+		case 4:
 			cout << "\nEnter the data: ";
 		    cin >> key;
             max_heap_insert(array, key);
+            cout << "\nSuccessfully inserted " << key << endl;
+            display();
 			break;
-		case 3:
+		case 5:
             key = heap_maximum(array);
             cout << "\nMaximum Key: " << key << endl;
 			break;
-		case 4:
+		case 6:
             key = heap_extract_max(array);
             if(key == -1000)
                 cout << "\nHeap Underflow!" << endl;
             else
                 cout << "\nExtracted maximum Key: " << key << endl;
+			break;
+		case 7:
+            display();
+			break;
+		case 0:
 			break;
 		default:
 			cout << "\n########### WRONG CHOICE... ###########\n";
@@ -171,11 +201,15 @@ void priorityQueue::sub_choiceCalling_1(int ch1)
 
 void priorityQueue::sub_options_2()
 {
-	cout << "\n------- SUB MENU -------";
-    cout << "\n1. HEAP DECREASE KEY"
-		<< "\n2. MIN HEAP INSERT"
-		<< "\n3. HEAP MINIMUM"
-		<< "\n4. HEAP EXTRACT MIN";
+	cout << "\n------- MENU -------";
+    cout <<	"\n1. MIN HEAPIFY"
+		<< "\n2. BUILD MIN HEAP"
+        << "\n3. HEAP DECREASE KEY"
+		<< "\n4. MIN HEAP INSERT"
+		<< "\n5. HEAP MINIMUM"
+		<< "\n6. HEAP EXTRACT MIN"
+		<< "\n7. DISPLAY"
+		<< "\n0. EXIT";
 }
 
 void priorityQueue::sub_choiceCalling_2(int ch1)
@@ -184,28 +218,57 @@ void priorityQueue::sub_choiceCalling_2(int ch1)
     int key;
     switch(ch1)
 	{
-		case 1:
-			cout << "\nEnter index to be decreased: ";
+        case 1:
+			cout << "\nEnter index from " << floor(size/2) << " upto 1: ";
 		    cin >> i;
-			cout << "\nEnter the data: ";
-		    cin >> key;
-            heap_decrease_key(array, i, key);
+            if(i <= floor(size/2) && i >= 1)
+            {
+                min_heapify(array, i);
+                cout << "\nSuccessfully Min Heapified at " << i << "!\n";
+                display();
+            }
+            else
+                cout << "\n########### WRONG CHOICE... ###########\n";
 			break;
 		case 2:
+			build_min_heap();
+            cout << "\nSuccessfully built Min Heap!\n";
+            display();
+			break;
+		case 3:
+			cout << "\nEnter index to be decreased: ";
+		    cin >> i;
+            if(i <= size && i >= 1)
+            {
+                cout << "\nEnter the data: ";
+                cin >> key;
+                heap_decrease_key(array, i, key);
+            }
+            else
+                cout << "\n########### WRONG CHOICE... ###########\n";
+			break;
+		case 4:
 			cout << "\nEnter the data: ";
 		    cin >> key;
             min_heap_insert(array, key);
+            cout << "\nSuccessfully inserted " << key << endl;
+            display();
 			break;
-		case 3:
+		case 5:
             key = heap_minimum(array);
             cout << "\nMinimum Key: " << key << endl;
 			break;
-		case 4:
+		case 6:
             key = heap_extract_min(array);
             if(key == -1000)
                 cout << "\nHeap Underflow!" << endl;
             else
                 cout << "\nExtracted minimum Key: " << key << endl;
+			break;
+		case 7:
+            display();
+			break;
+		case 0:
 			break;
 		default:
 			cout << "\n########### WRONG CHOICE... ###########\n";
@@ -288,6 +351,8 @@ void priorityQueue::heap_increase_key(int *array, int i, int key)
         i = parent;
         parent = floor(i/2);
     }
+    cout << "\nSuccessfully increased " << i << "th element to " << key << endl;
+    display();
 }
 
 void priorityQueue::max_heap_insert(int *array, int key)
@@ -316,7 +381,7 @@ void priorityQueue::heap_decrease_key(int *array, int i, int key)
 {
     if(key > array[i])
     {
-        cout << "\nData entered is greater than " << i << "th elemnt " << array[i] << endl;
+        cout << "\nData entered is greater than " << i << "th element " << array[i] << endl;
         return;
     }
     int parent = floor(i/2);
@@ -327,6 +392,8 @@ void priorityQueue::heap_decrease_key(int *array, int i, int key)
         i = parent;
         parent = floor(i/2);
     }
+    cout << "\nSuccessfully decreased " << i << "th element to " << key << endl;
+    display();
 }
 
 void priorityQueue::min_heap_insert(int *array, int key)
@@ -351,24 +418,48 @@ int priorityQueue::heap_extract_min(int *array)
     return minim;
 }
 
+void priorityQueue::heap_sort(int *array)
+{
+    int s = size;
+    build_max_heap();
+    for(int i=size; i>=2; i--)
+    {
+        array = swap(array, 1, i);
+        size--;
+        max_heapify(array, 1);
+    }
+    size = s;
+}
+
+void priorityQueue::display()
+{
+    cout << "\nArray: ";
+    if(size != 0)
+        for(int x=1; x<=size; x++)
+            cout << array[x] << " ";
+    else
+        cout << "Empty!";
+    cout << endl;
+}
+
 int main()
 {
     int choice;
 	priorityQueue ob;
 	do
 	{
-		//cout << "\n=========== PRIORITY QUEUE ===========\n";
+		cout << "\n=========== PRIORITY QUEUE ===========\n";
 		ob.options();
 		choice = ob.choice();
 		if(ob.ch == 0)
 			break;
-		//system("clear");
-		//cout << "\n=========== PRIORITY QUEUE ===========\n";
+		system("clear");
+		cout << "\n=========== PRIORITY QUEUE ===========\n";
 		ob.choiceCalling(choice);
-		//cout << "\nPress Enter to continue...";
-		//cin.ignore();
-		//getchar();
-		//system("clear");
+		cout << "\nPress Enter to continue...";
+		cin.ignore();
+		getchar();
+		system("clear");
 	}while(1);
 	
 	return 0;
