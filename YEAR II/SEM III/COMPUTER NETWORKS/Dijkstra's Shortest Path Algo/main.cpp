2109f707e0e80f19f9de18a5a2a1538dc9800b16
Shubhang Gupta <shubhsahu1805@gmail.com>
2020-11-11 15:46:33 +0000

    final;

--- YEAR II/SEM III/COMPUTER NETWORKS/Dijkstra's Shortest Path Algo/main.cpp
+++ YEAR II/SEM III/COMPUTER NETWORKS/Dijkstra's Shortest Path Algo/main.cpp
@@ -12,46 +12,52 @@ class dijkstra_algo
         int paths;
 
     public:
+        int path[MAX];
         bool visited[MAX];
         int distance[MAX];
         int ADJmatrix[MAX][MAX];
 
         dijkstra_algo(int, int);
 
-        void ADJgraph(int, int);
-        void show_graph(int);
+        void ADJgraph();
+        void show_graph();
         void dijkstra(int);
+        int find_min_dist();
+        void display_shortest_path(int);
 };
 
 dijkstra_algo::dijkstra_algo(int r, int p)
 {
     routers = r;
     paths = p;
+    for(int i=0; i<routers; i++)
+        for(int j=0; j<routers; j++)
+            ADJmatrix[i][j] = 0;
 }
 
-void dijkstra_algo::ADJgraph(int r, int p)
+void dijkstra_algo::ADJgraph()
 {
     int src, des, dis;
-    for(int i=0; i<p; i++)
+    for(int i=0; i<paths; i++)
     {
-        cout << "\nRouter " << i+1;
-        cout << "\n========";
+        cout << "\nPath " << i+1;
+        cout << "\n======";
         cout << "\nEnter source router: ";
         cin >> src;
-        cout << "\nEnter destination router: ";
+        cout << "Enter destination router: ";
         cin >> des;
-        cout << "\nEnter distace of path: ";
+        cout << "Enter distace of path: ";
         cin >> dis;
         ADJmatrix[src-1][des-1] = dis;
         ADJmatrix[des-1][src-1] = dis;
     }
 }
 
-void dijkstra_algo::show_graph(int r)
+void dijkstra_algo::show_graph()
 {
-    for(int i=0; i<r; i++)
+    for(int i=0; i<routers; i++)
     {
-        for(int j=0; j<r; j++)
+        for(int j=0; j<routers; j++)
             cout << setw(5) << ADJmatrix[i][j];
         cout << endl;
     }
@@ -66,22 +72,68 @@ void dijkstra_algo::dijkstra(int src)
     }
 
     path[src] = -1;
+    distance[src] = 0;
     
+    for(int i=0; i<routers-1; i++)
+    {
+        int minim = find_min_dist();
+        visited[minim] = true;
+        for(int j=0; j<routers; j++)
+        {
+            if(ADJmatrix[minim][j] && 
+               !visited[j] &&
+               distance[minim] + ADJmatrix[minim][j] < distance[j])
+            {
+                path[j] = minim;
+                distance[j] = distance[minim] + ADJmatrix[minim][j];
+            }
+        }
+    }
+    
+    cout << "\nDest Router\tDistance\tShortest Path\n==============================================";
+    for(int i=0; i<routers; i++)
+    {
+        cout << endl << i+1 << "\t\t" << distance[i] << "\t\t" << (src+1);
+        display_shortest_path(i);
+    }
+    cout << endl;
+}
+
+int dijkstra_algo::find_min_dist()
+{
+    int minim = -1;
+
+    for(int i=0; i<routers; i++)
+        if(!visited[i] && (minim == -1 || distance[i] < distance[minim]))
+            minim = i;
+    
+    return minim;
+}
+
+void dijkstra_algo::display_shortest_path(int indx)
+{
+    if (path[indx] == -1)
+        return;
+    display_shortest_path(path[indx]);
+    cout << " -> " << (indx + 1);
 }
 
 int main()
 {
+    cout << "\n============ DIJKSTRA'S SHORTEST PATH ============";
     int r, p;
     cout << "\nEnter the number of routers: ";
     cin >> r;
-    cout << "\nEnter the number of paths: ";
+    cout << "Enter the number of paths: ";
     cin >> p;
 
     dijkstra_algo ob(r, p);
-    ob.ADJgraph(r, p);
-    ob.show_graph(r);
+    ob.ADJgraph();
+    
+    cout << "\nAdjacency Matrix\n================\n";
+    ob.show_graph();
 
     cout << "\nEnter the source router: ";
     cin >> r;
-    ob.dijkstra(r);
+    ob.dijkstra(r-1);
 }
