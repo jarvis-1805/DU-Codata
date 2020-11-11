5fdaa9a0e27e83702b112bb8d72ff1d1b25ffe23
Shubhang Gupta <shubhsahu1805@gmail.com>
2020-11-11 12:00:08 +0000

    Updated;

--- YEAR II/SEM III/COMPUTER NETWORKS/Dijkstra's Shortest Path Algo/main.cpp
+++ YEAR II/SEM III/COMPUTER NETWORKS/Dijkstra's Shortest Path Algo/main.cpp
@@ -1,51 +1,87 @@
 #include <iostream>
-#define MAX 100
+#include <climits>
+#include <iomanip>
+#define MAX 10
 
 using namespace std;
 
-class dijkstra
+class dijkstra_algo
 {
+    private:
+        int routers;
+        int paths;
+
     public:
-        int n;
-        int path[MAX][MAX];
+        bool visited[MAX];
+        int distance[MAX];
+        int ADJmatrix[MAX][MAX];
 
-        dijkstra();
+        dijkstra_algo(int, int);
 
-        void input();
-        void show_path();
+        void ADJgraph(int, int);
+        void show_graph(int);
+        void dijkstra(int);
 };
 
-dijkstra::dijkstra()
+dijkstra_algo::dijkstra_algo(int r, int p)
 {
-    cout << "\nEnter the number of routers: ";
-    cin >> n;
+    routers = r;
+    paths = p;
 }
 
-void dijkstra::input()
+void dijkstra_algo::ADJgraph(int r, int p)
 {
-    for(int i=0; i<n-1; i++)
+    int src, des, dis;
+    for(int i=0; i<p; i++)
     {
-        for(int j=i+1; j<n; j++)
-        {
-            cout << "\nEnter distance b/w routers " << i+1 << " and " << j+1 << ": ";
-            cin >> path[i][j];
-            path[j][i] = path[i][j];
-        }
+        cout << "\nRouter " << i+1;
+        cout << "\n========";
+        cout << "\nEnter source router: ";
+        cin >> src;
+        cout << "\nEnter destination router: ";
+        cin >> des;
+        cout << "\nEnter distace of path: ";
+        cin >> dis;
+        ADJmatrix[src-1][des-1] = dis;
+        ADJmatrix[des-1][src-1] = dis;
     }
 }
 
-void dijkstra::show_path()
+void dijkstra_algo::show_graph(int r)
 {
-    for(int i=0; i<n; i++)
+    for(int i=0; i<r; i++)
     {
-        for(int j=0; j<n; j++)
-            cout << path[i][j] << "  ";
+        for(int j=0; j<r; j++)
+            cout << setw(5) << ADJmatrix[i][j];
         cout << endl;
     }
 }
 
+void dijkstra_algo::dijkstra(int src)
+{
+    for(int i=0; i<routers; i++)
+    {
+        visited[i] = false;
+        distance[i] = INT_MAX;
+    }
+
+    path[src] = -1;
+    
+}
+
 int main()
 {
-    dijkstra ob;
-    ob.input();
+    int r, p;
+    cout << "\nEnter the number of routers: ";
+    cin >> r;
+    cout << "\nEnter the number of paths: ";
+    cin >> p;
+
+    dijkstra_algo ob(r, p);
+    ob.ADJgraph(r, p);
+    ob.show_graph(r);
+
+    cout << "\nEnter the source router: ";
+    cin >> r;
+    ob.dijkstra(r);
 }
