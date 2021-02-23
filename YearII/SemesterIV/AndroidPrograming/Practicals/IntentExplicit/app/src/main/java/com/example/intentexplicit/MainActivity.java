99dbf173b41c0fe3e065c356fa35a87b9be76d78
jarvis-1805 <shubhsahu1805@gmail.com>
2021-02-24 01:18:37 +0530

    fixes

--- YearII/SemesterIV/AndroidPrograming/Practicals/IntentExplicit/app/src/main/java/com/example/intentexplicit/MainActivity.java
+++ YearII/SemesterIV/AndroidPrograming/Practicals/IntentExplicit/app/src/main/java/com/example/intentexplicit/MainActivity.java
@@ -4,25 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtTxt;
+    private String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edtTxt = findViewById(R.id.editText);
-        Button send = findViewById(R.id.button);
+        edtTxt = findViewById(R.id.editText);
+        Button sendBtn = findViewById(R.id.button);
 
-        send.setOnClickListener(v -> {
-            String data = edtTxt.getText().toString();
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            intent.putExtra("Data", data);
-            startActivity(intent);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = edtTxt.getText().toString();
+                if(!data.equals("")) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("Data", data);
+                    startActivity(intent);
+                } else {
+                    edtTxt.setError("Please enter your name!");
+                }
+            }
         });
-
     }
 }
--- YearII/SemesterIV/AndroidPrograming/Practicals/IntentExplicit/app/src/main/java/com/example/intentexplicit/SecondActivity.java
+++ YearII/SemesterIV/AndroidPrograming/Practicals/IntentExplicit/app/src/main/java/com/example/intentexplicit/SecondActivity.java
@@ -4,28 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class SecondActivity extends AppCompatActivity {
-
-    TextView txtView;
-    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        txtView = findViewById(R.id.tv4);
+        TextView txtView = findViewById(R.id.tv4);
+        Button back = findViewById(R.id.backButton);
 
         Intent intent = getIntent();
-        data = intent.getStringExtra("Data");
-        if(!data.equals("")){
-            txtView.setText(data);
-        }
-        else{
-            txtView.setText(R.string.Null);
-        }
+        String data = intent.getStringExtra("Data");
+        txtView.setText(data);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
