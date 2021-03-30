32e33600cd02011905ea5aecab51af04c252badb
jarvis-1805 <shubhsahu1805@gmail.com>
2021-03-30 23:47:26 +0530

    fixes

--- YearII/SemesterIV/AndroidPrograming/Practicals/SelectMenu/app/src/main/java/com/example/selectmenu/MainActivity.java
+++ YearII/SemesterIV/AndroidPrograming/Practicals/SelectMenu/app/src/main/java/com/example/selectmenu/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.selectmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(item.getTitle().toString().toUpperCase());
+
+        return super.onOptionsItemSelected(item);
     }
 }
