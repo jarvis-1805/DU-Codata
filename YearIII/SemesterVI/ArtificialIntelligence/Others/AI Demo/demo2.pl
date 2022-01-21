ac107ce2aa3a1a240df34da40b728b7a082a4e04
jarvis-1805 <shubhsahu1805@gmail.com>
2022-01-21 15:07:01 +0530

    fixes

--- YearIII/SemesterVI/ArtificialIntelligence/Others/AI Demo/demo2.pl
+++ YearIII/SemesterVI/ArtificialIntelligence/Others/AI Demo/demo2.pl
@@ -8,7 +8,7 @@ a :- b, c, d.
 d :- e.
 f :- b, (c, not(e)).
 hello :- write('Hello from Prolog!\n').
-main :- ,X = 1
+main :- X = 1,
           Y is X + 1,
           format('~w ~w.\n', ['X is', X]),
           format('~w ~w.\n', ['Y is', Y]).
